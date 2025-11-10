aaf58119a26c7c59849b154e943c6df401fa8d3a
Kendrick Wong <kendrickw@gmail.com>
2025-11-10 17:39:11 -0500

    fix: rename repo to deploy to root

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   // Deploying static site to github.io
   output: 'export',
   // Optional: if deploying to a subdirectory like https://username.github.io/your-repo-name
-  basePath: '/kendrickw-page',
-  assetPrefix: '/kendrickw-page',
+  // basePath: '/your-repo-name',
+  // assetPrefix: '/your-repo-name',
 };
 
 export default nextConfig;
